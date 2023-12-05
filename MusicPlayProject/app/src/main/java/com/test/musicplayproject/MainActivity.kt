e9eb65dc555e1296ed27b371378cf1d8c127ae2f
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2023-12-05 18:29:05 +0900

    feat: mp3 음원 파일 play, pause, stop구현
    백그라운드 재생 구현 예정

--- MusicPlayProject/app/src/main/java/com/test/musicplayproject/MainActivity.kt
+++ MusicPlayProject/app/src/main/java/com/test/musicplayproject/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.test.musicplayproject
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.test.musicplayproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var actiivityMainBinding: ActivityMainBinding
+    var mediaPlayer:MediaPlayer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        actiivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(actiivityMainBinding.root)
+
+        actiivityMainBinding.run {
+            buttonStart.setOnClickListener {
+                mediaPlayerPlay()
+            }
+            buttonStop.setOnClickListener {
+                mediaPlayerStop()
+            }
+            buttonPause.setOnClickListener {
+                mediaPlayerPause()
+            }
+        }
+    }
+
+    fun mediaPlayerPlay(){
+        if (mediaPlayer == null){
+            mediaPlayer = MediaPlayer.create(this, R.raw.cheer)
+        }
+        mediaPlayer?.start()
+    }
+    fun mediaPlayerStop(){
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
+        mediaPlayer = null
+    }
+    fun mediaPlayerPause(){
+        mediaPlayer?.pause()
     }
 }
---
+++ MusicPlayProject/app/src/main/java/com/test/musicplayproject/MediaPlayerService.kt
@@ -0,0 +1,12 @@
+package com.test.musicplayproject
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MediaPlayerService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
