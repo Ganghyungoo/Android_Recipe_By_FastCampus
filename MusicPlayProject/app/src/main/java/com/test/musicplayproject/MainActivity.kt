d351f103f1883ef4ddda9058cfd9725e564745c0
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2023-12-20 20:25:00 +0900

    feat: MusiicPlayProject 포그라운드 재생 및 Notification 구현 완료(~api 26)
    api33이상 기기 대응 필요한 상태

--- MusicPlayProject/app/src/main/java/com/test/musicplayproject/MainActivity.kt
+++ MusicPlayProject/app/src/main/java/com/test/musicplayproject/MainActivity.kt
@@ -47,4 +47,9 @@ class MainActivity : AppCompatActivity() {
         }
         startService(intent)
     }
+
+    override fun onDestroy() {
+        stopService(Intent(this,MediaPlayerService::class.java))
+        super.onDestroy()
+    }
 }
--- MusicPlayProject/app/src/main/java/com/test/musicplayproject/MediaPlayerService.kt
+++ MusicPlayProject/app/src/main/java/com/test/musicplayproject/MediaPlayerService.kt
@@ -1,14 +1,106 @@
 package com.test.musicplayproject
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.graphics.drawable.Icon
 import android.media.MediaPlayer
+import android.os.Build
 import android.os.IBinder
+import androidx.annotation.RequiresApi
 
 class MediaPlayerService : Service() {
     var mediaPlayer: MediaPlayer? = null
     override fun onBind(intent: Intent): IBinder? {
         return null
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate() {
+        super.onCreate()
+        creatNotificationChannel()
+
+        val playIcon = Icon.createWithResource(baseContext,R.drawable.baseline_play_arrow_24)
+        val pauseIcon = Icon.createWithResource(baseContext,R.drawable.baseline_pause_24)
+        val stopIcon = Icon.createWithResource(baseContext,R.drawable.baseline_stop_24)
+
+        val mainPendingIntent =  PendingIntent.getActivity(
+            baseContext,
+            0,
+            Intent(baseContext,MainActivity::class.java)
+                .apply {
+                    setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                },
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val pausePendingInent = PendingIntent.getService(
+            baseContext,
+            0,
+            Intent(baseContext, MediaPlayerService::class.java).apply { action = MEDIA_PLAYER_PAUSE },
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        val playPendingInent = PendingIntent.getService(
+            baseContext,
+            1,
+            Intent(baseContext, MediaPlayerService::class.java).apply { action = MEDIA_PLAYER_PLAY },
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        val stopPendingInent = PendingIntent.getService(
+            baseContext,
+            2,
+            Intent(baseContext, MediaPlayerService::class.java).apply { action = MEDIA_PLAYER_STOP },
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+
+
+        val notification = Notification.Builder(baseContext, CHANNEL_ID)
+            .setStyle(
+                Notification.MediaStyle()
+                    .setShowActionsInCompactView(0,1,2)
+            )
+            .setVisibility(Notification.VISIBILITY_PUBLIC)
+            .setSmallIcon(R.drawable.baseline_star_24)
+            .addAction(
+                Notification.Action.Builder(
+                    pauseIcon,
+                    "Pause",
+                    pausePendingInent
+                ).build()
+            )
+            .addAction(
+                Notification.Action.Builder(
+                    playIcon,
+                    "Play",
+                    playPendingInent
+                ).build()
+            )
+            .addAction(
+                Notification.Action.Builder(
+                    stopIcon,
+                    "Stop",
+                    stopPendingInent
+                ).build()
+            )
+            .setContentIntent(mainPendingIntent)
+            .setContentTitle("음악재생")
+            .setContentText("음악 앱이 실행 중 입니다")
+            .build()
+        startForeground(100, notification)
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun creatNotificationChannel(){
+        val channel = NotificationChannel(CHANNEL_ID,"MEDIA_PLAYER", NotificationManager.IMPORTANCE_DEFAULT)
+
+        val notificationManager = baseContext.getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(channel)
+
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -30,5 +122,14 @@ class MediaPlayerService : Service() {
             }
         }
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        mediaPlayer?.apply {
+            stop()
+            release()
+        }
+        mediaPlayer = null
+        super.onDestroy()
     }
 }
