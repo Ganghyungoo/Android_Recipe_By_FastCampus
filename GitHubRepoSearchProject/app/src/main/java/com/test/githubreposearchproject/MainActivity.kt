0522e3c222b69d8009d4237a362586df63b739fb
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2024-01-05 18:44:21 +0900

    Revert "fear: 깃허브 사용자 검색 및 사용자의 레포지토리 검색 레트로핏 통신 구현(테스트 완료)"
    This reverts commit fe673b32ad8df80af557e0045baa45b17725d9d3.

--- GitHubRepoSearchProject/app/src/main/java/com/test/githubreposearchproject/MainActivity.kt
+++ GitHubRepoSearchProject/app/src/main/java/com/test/githubreposearchproject/MainActivity.kt
@@ -2,46 +2,10 @@ package com.test.githubreposearchproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.test.githubreposearchproject.datamodel.Repo
-import com.test.githubreposearchproject.datamodel.UserDto
-import com.test.githubreposearchproject.network.GithubService
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl(" https://api.github.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-        val githubService = retrofit.create(GithubService::class.java)
-        githubService.listRepos("square").enqueue(object : Callback<List<Repo>> {
-            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
-                Log.d("testt", "List Repo: ${response.body().toString()}")
-            }
-
-            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
-                TODO("Not yet implemented")
-            }
-
-        })
-        githubService.searchUsers("squar").enqueue(object :Callback<UserDto>{
-            override fun onResponse(call: Call<UserDto>, response: Response<UserDto>) {
-                Log.d("testt", "Search User: ${response.body().toString()}")
-            }
-
-            override fun onFailure(call: Call<UserDto>, t: Throwable) {
-                TODO("Not yet implemented")
-            }
-
-        })
     }
-
 }
