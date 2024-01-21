c6fef3f3eae737d2e4706c7f1d62706aca2a4b5a
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2024-01-22 02:02:54 +0900

    feat: 다른 사용자에게 이모지를 전송했을 때 해당 사용자에게 리엑션이 보이도록 ChildEventListener 구현

--- ZenlyAppProject/app/src/main/java/com/test/zenlyappproject/MapActivity.kt
+++ ZenlyAppProject/app/src/main/java/com/test/zenlyappproject/MapActivity.kt
@@ -2,12 +2,21 @@ package com.test.zenlyappproject
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
+import android.view.RoundedCorner
+import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
@@ -18,23 +27,29 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.test.zenlyappproject.databinding.ActivityMapBinding
 import com.test.zenlyappproject.dataclass.Person
 
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
     private lateinit var activityMapBinding: ActivityMapBinding
     private lateinit var googleMap: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var trakingPersonUid: String = ""
     private val markerMap = hashMapOf<String,Marker>()
 
     private val locationCallback = object:LocationCallback(){
@@ -86,9 +101,25 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this@MapActivity)
 
+
         requestLocationPermission()
+        setUpEmojiAnimation()
         setUpFirebaseDatabase()
 
+    }
+
+    private fun setUpEmojiAnimation() {
+        activityMapBinding.emojiLottieAnimationView.setOnClickListener {
+            if (trakingPersonUid != "") {
+                val lastEmoji = mutableMapOf<String, Any>()
+                lastEmoji["type"] = "dafault"
+                lastEmoji["lastModifier"] = System.currentTimeMillis()
+                Firebase.database.reference.child("Emoji").child(trakingPersonUid)
+                    .updateChildren(lastEmoji)
+            }
+            activityMapBinding.emojiLottieAnimationView.playAnimation()
+        }
+        activityMapBinding.centerLottieAnimationView.speed = 3f
     }
 
     private fun getCurrentLocation(){
@@ -143,6 +174,17 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
                      markerMap[uid]?.position = LatLng(person.latitude ?: 0.0,person.longitude ?: 0.0)
                      Log.d("testt","위치 업데이트중")
                  }
+
+                 if (uid == trakingPersonUid){
+                     googleMap.animateCamera(
+                         CameraUpdateFactory.newCameraPosition(
+                             CameraPosition.Builder()
+                                 .target(LatLng(person.latitude ?: 0.0, person.longitude ?: 0.0))
+                                 .zoom(16.0f)
+                                 .build()
+                         )
+                     )
+                 }
              }
 
              override fun onChildRemoved(snapshot: DataSnapshot) {
@@ -158,6 +200,41 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
              }
 
          })
+
+        Firebase.database.reference.child("Emoji").child(Firebase.auth.currentUser?.uid ?: "").addChildEventListener(
+            object: ChildEventListener{
+                override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                    activityMapBinding.centerLottieAnimationView.playAnimation()
+                    activityMapBinding.centerLottieAnimationView.animate()
+                        .scaleX(7f)
+                        .scaleY(7f)
+                        .alpha(0.3f)
+                        .setDuration(activityMapBinding.centerLottieAnimationView.duration / 3)
+                        .withEndAction{
+                            activityMapBinding.centerLottieAnimationView.scaleX = 0f
+                            activityMapBinding.centerLottieAnimationView.scaleY = 0f
+                            activityMapBinding.centerLottieAnimationView.alpha = 1f
+                        }
+                }
+
+                override fun onChildRemoved(snapshot: DataSnapshot) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            }
+        )
     }
 
     private fun makeNewMarker(person: Person): Marker? {
@@ -167,6 +244,38 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
                 .title(person.name.orEmpty())
         ) ?: return null
 
+        marker.tag = person.uid
+        
+        Glide.with(this).asBitmap()
+            .load(person.profilePhoto)
+            .transform(RoundedCorners(60))
+            .override(200)
+            .listener(object : RequestListener<Bitmap> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Bitmap>,
+                    isFirstResource: Boolean,
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Bitmap,
+                    model: Any,
+                    target: Target<Bitmap>?,
+                    dataSource: DataSource,
+                    isFirstResource: Boolean,
+                ): Boolean {
+                    runOnUiThread {
+                        marker.setIcon( BitmapDescriptorFactory.fromBitmap(
+                            resource
+                        ))
+                    }
+                    return true
+                }
+            }).submit()
+
         return marker
     }
 
@@ -175,6 +284,12 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
         googleMap.setMaxZoomPreference(20.0f)
         googleMap.setMinZoomPreference(10.0f)
+
+        googleMap.setOnMarkerClickListener(this@MapActivity)
+
+        googleMap.setOnMapClickListener {
+            trakingPersonUid = ""
+        }
     }
 
     override fun onResume() {
@@ -185,5 +300,12 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onStop() {
         super.onStop()
         fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    override fun onMarkerClick(p0: Marker): Boolean {
+        val bottomSheetBehavior = BottomSheetBehavior.from(activityMapBinding.emojiBottomSheetLayout)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        trakingPersonUid = p0.tag as String
+        return false
     }
 }
