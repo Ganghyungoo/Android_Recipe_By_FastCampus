ff1719c11cf9f03b6798355317809331af459d2e
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2024-02-03 00:29:17 +0900

    feat: 커스텀 뷰를 통한 addView구현 완료

--- MotionAnimationProject/app/src/main/java/com/test/motionanimationproject/HomeFragment.kt
+++ MotionAnimationProject/app/src/main/java/com/test/motionanimationproject/HomeFragment.kt
@@ -19,10 +19,18 @@ class HomeFragment : Fragment() {
         fragmentHomeBinding = FragmentHomeBinding.inflate(layoutInflater)
 
         //파일 데이터를 읽는 작업
-        val homeData = context?.readData() ?: return fragmentHomeBinding.root
+        val homeData =
+            context?.readData("home.json", Home::class.java) ?: return fragmentHomeBinding.root
+        val menuData =
+            context?.readData("menu.json", Menu::class.java) ?: return fragmentHomeBinding.root
         //툴바 닉네임 설정
-        fragmentHomeBinding.toolBarTitleTextView.text = getString(R.string.toolbar_title_text, homeData.user.nickName)
-        fragmentHomeBinding.starCountTextView.text = getString(R.string.toolbar_star_count, homeData.user.starCount, homeData.user.totalCount)
+        fragmentHomeBinding.toolBarTitleTextView.text =
+            getString(R.string.toolbar_title_text, homeData.user.nickName)
+        fragmentHomeBinding.starCountTextView.text = getString(
+            R.string.toolbar_star_count,
+            homeData.user.starCount,
+            homeData.user.totalCount
+        )
 
         fragmentHomeBinding.progressHorizontal.max = homeData.user.totalCount
         fragmentHomeBinding.progressHorizontal.progress = homeData.user.starCount
@@ -31,7 +39,17 @@ class HomeFragment : Fragment() {
             .load(homeData.appBarImage)
             .into(fragmentHomeBinding.toolBarImageView)
 
-        Log.d("testt", homeData.appBarImage)
+
+        fragmentHomeBinding.recommendMenuList.titleTextView.text = getString(R.string.recommend_title,homeData.user.nickName)
+        menuData.coffee.forEach { coffee ->
+            fragmentHomeBinding.recommendMenuList.menuLayout.addView(
+                MenuView(context = requireContext()).apply {
+                    setTitle(coffee.name)
+                    setImageView(coffee.image)
+                }
+            )
+        }
+
         return fragmentHomeBinding.root
     }
 
