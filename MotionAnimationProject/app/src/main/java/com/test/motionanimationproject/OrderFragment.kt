06949b81b0453ae45ae463949f7a8c5862b09407
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2024-02-07 19:16:45 +0900

    feat: order 화면 구현 완료(모션 레이아웃)

--- MotionAnimationProject/app/src/main/java/com/test/motionanimationproject/OrderFragment.kt
+++ MotionAnimationProject/app/src/main/java/com/test/motionanimationproject/OrderFragment.kt
@@ -1,12 +1,15 @@
 package com.test.motionanimationproject
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.test.motionanimationproject.databinding.FragmentHomeBinding
 import com.test.motionanimationproject.databinding.FragmentOrderBinding
+import kotlin.math.abs
 
 class OrderFragment : Fragment() {
     private lateinit var fragmentHomeBinding: FragmentOrderBinding
@@ -15,6 +18,24 @@ class OrderFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         fragmentHomeBinding = FragmentOrderBinding.inflate(layoutInflater)
+
+        val menuAdapter = MenuAdapter()
+
+        val menuData = context?.readData("menu.json", Menu::class.java) ?: return fragmentHomeBinding.root
+        menuAdapter.apply {
+            submitList(menuData.coffee)
+        }
+
+        fragmentHomeBinding.orderRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = menuAdapter
+        }
+
+        fragmentHomeBinding.toolbarLayout.addOnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val seekPosition = abs(verticalOffset) / appBarLayout.totalScrollRange.toFloat()
+            fragmentHomeBinding.orderMotionLayout.progress = seekPosition
+            Log.d("orderToolbar","toolBarTotalRange:${appBarLayout.totalScrollRange} / $verticalOffset")
+        }
         return fragmentHomeBinding.root
     }
 
