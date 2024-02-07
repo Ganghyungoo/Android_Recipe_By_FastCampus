a7f32c52d78af6ce993cefa72052268de59b6e4b
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2024-02-07 16:45:03 +0900

    feat: 모션애니메이션 앱 홈화면 구현 완료
    ++order화면 구현 중

--- MotionAnimationProject/app/src/main/java/com/test/motionanimationproject/Home.kt
+++ MotionAnimationProject/app/src/main/java/com/test/motionanimationproject/Home.kt
@@ -2,11 +2,17 @@ package com.test.motionanimationproject
 
 data class Home(
     val user: User,
-    val appBarImage: String
+    val appBarImage: String,
+    val banner: Banner
 )
 
 data class User(
     val nickName: String,
     val starCount: Int,
     val totalCount: Int
+)
+
+data class Banner(
+    val image :String,
+    val contentDescription: String
 )
--- MotionAnimationProject/app/src/main/java/com/test/motionanimationproject/HomeFragment.kt
+++ MotionAnimationProject/app/src/main/java/com/test/motionanimationproject/HomeFragment.kt
@@ -1,46 +1,76 @@
 package com.test.motionanimationproject
 
+import android.animation.ValueAnimator
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.bumptech.glide.Glide
 import com.test.motionanimationproject.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
     private lateinit var fragmentHomeBinding: FragmentHomeBinding
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         // Inflate the layout for this fragment
-        fragmentHomeBinding = FragmentHomeBinding.inflate(layoutInflater)
+        fragmentHomeBinding = FragmentHomeBinding.bind(view)
 
         //파일 데이터를 읽는 작업
         val homeData =
-            context?.readData("home.json", Home::class.java) ?: return fragmentHomeBinding.root
+            context?.readData("home.json", Home::class.java) ?: return
         val menuData =
-            context?.readData("menu.json", Menu::class.java) ?: return fragmentHomeBinding.root
-        //툴바 닉네임 설정
-        fragmentHomeBinding.toolBarTitleTextView.text =
-            getString(R.string.toolbar_title_text, homeData.user.nickName)
-        fragmentHomeBinding.starCountTextView.text = getString(
-            R.string.toolbar_star_count,
-            homeData.user.starCount,
-            homeData.user.totalCount
-        )
-
-        fragmentHomeBinding.progressHorizontal.max = homeData.user.totalCount
-        fragmentHomeBinding.progressHorizontal.progress = homeData.user.starCount
-
-        Glide.with(fragmentHomeBinding.toolBarImageView)
-            .load(homeData.appBarImage)
-            .into(fragmentHomeBinding.toolBarImageView)
+            context?.readData("menu.json", Menu::class.java) ?: return
+        initToolbar(homeData)
+        //커피 추천 메뉴
+        initCoffeeRecommend(homeData, menuData)
+        //베너 설정
+        initBanner(homeData)
+        //음식(디저트) 추천 메뉴
+        initFoodRecommend(menuData)
+        //플로팅 버튼 설정
+        initFloatingButtonState()
 
 
-        fragmentHomeBinding.recommendMenuList.titleTextView.text = getString(R.string.recommend_title,homeData.user.nickName)
+
+    }
+
+    private fun initFloatingButtonState() {
+        fragmentHomeBinding.scrollView.setOnScrollChangeListener { _, _, scrollY, _, oldScrollY ->
+            if (scrollY == 0) {
+                fragmentHomeBinding.floatingActionButton.extend()
+            } else {
+                fragmentHomeBinding.floatingActionButton.shrink()
+            }
+        }
+    }
+
+    private fun initFoodRecommend(menuData: Menu) {
+        fragmentHomeBinding.foodMenuList.titleTextView.text = getString(R.string.food_title)
+        menuData.food.forEach { food ->
+            fragmentHomeBinding.foodMenuList.menuLayout.addView(
+                MenuView(context = requireContext()).apply {
+                    setTitle(food.name)
+                    setImageView(food.image)
+                }
+            )
+        }
+    }
+
+    private fun initBanner(homeData: Home) {
+        fragmentHomeBinding.bannerImageViewLayout.bannerImageView.apply {
+            Glide.with(this)
+                .load(homeData.banner.image)
+                .into(this)
+            this.contentDescription = homeData.banner.contentDescription
+        }
+    }
+
+    private fun initCoffeeRecommend(
+        homeData: Home,
+        menuData: Menu,
+    ) {
+        fragmentHomeBinding.recommendMenuList.titleTextView.text =
+            getString(R.string.recommend_title, homeData.user.nickName)
         menuData.coffee.forEach { coffee ->
             fragmentHomeBinding.recommendMenuList.menuLayout.addView(
                 MenuView(context = requireContext()).apply {
@@ -49,8 +79,31 @@ class HomeFragment : Fragment() {
                 }
             )
         }
+    }
 
-        return fragmentHomeBinding.root
+    private fun initToolbar(homeData: Home) {
+        //툴바 설정
+        fragmentHomeBinding.toolBarTitleTextView.text =
+            getString(R.string.toolbar_title_text, homeData.user.nickName)
+        fragmentHomeBinding.starCountTextView.text = getString(
+            R.string.toolbar_star_count,
+            homeData.user.starCount,
+            homeData.user.totalCount
+        )
+        fragmentHomeBinding.progressHorizontal.max = homeData.user.totalCount
+        fragmentHomeBinding.progressHorizontal.progress = homeData.user.starCount
+
+        Glide.with(fragmentHomeBinding.toolBarImageView)
+            .load(homeData.appBarImage)
+            .into(fragmentHomeBinding.toolBarImageView)
+
+        ValueAnimator.ofInt(0,homeData.user.starCount).apply {
+            duration = 1000
+            addUpdateListener {
+                fragmentHomeBinding.progressHorizontal.progress = it.animatedValue as Int
+            }
+            start()
+        }
     }
 
 
