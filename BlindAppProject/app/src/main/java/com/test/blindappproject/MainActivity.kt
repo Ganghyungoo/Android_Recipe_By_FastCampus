10b1ddcb3ea8c711073a6d6479867fac96392991
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2024-03-16 17:13:20 +0900

    feat: 블라인드 앱 재생성

--- BlindAppProject/app/src/main/java/com/test/blindappproject/MainActivity.kt
+++ BlindAppProject/app/src/main/java/com/test/blindappproject/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.test.blindappproject
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
